09c525ce11f9e084fc3b84274a478e453c2218d0
zonetechonline2024-Juan <zonetechonline2024@gmail.com>
2026-06-28 10:37:10 +0200

    debug: add env diagnostic endpoint

--- tienda-tech/api/debug-env.js
+++ tienda-tech/api/debug-env.js
@@ -2,8 +2,10 @@
 module.exports = (req, res) => {
   res.status(200).json({
     resend_set:  !!process.env.RESEND_API_KEY,
+    resend_len:  (process.env.RESEND_API_KEY || '').length,
     stripe_set:  !!process.env.STRIPE_SECRET_KEY,
     github_set:  !!process.env.GITHUB_PAT,
     node_env:    process.env.NODE_ENV,
+    ts:          Date.now(),
   });
 };
